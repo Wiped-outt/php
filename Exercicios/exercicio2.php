38e71f3ff567741802ca2d4d8e88e654a6066a35
Wiped-outt <rphlthrr@gmail.com>
2023-08-11 12:46:53 -0300

    commit 01

---
+++ Exercicios/exercicio2.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sintaxe Geral</title>
+
+    <style>
+        .body {background-color: black;}
+        .destaque {color: blueviolet;}
+        .destaque2 {color: blue;}
+    </style>
+
+</head>
+<body>
+    
+    <h1 class="destaque">Trabalhando com PHP</h1>
+    <p>Programador: Thierry</p>
+    <hr>
+
+    <?php 
+        // Geração de texto (string)
+        echo "Chama Sesi-Senai !";
+
+        /* Geração de texto estruturado (com tags, atributos) */
+        echo "<h2>Gerando <span class='destaque'>HTML</span> através do PHP.</h2>";
+
+        echo "<h2>Gerando <span class=\"destaque\">HTML</span> através do PHP.</h2>";
+
+    ?>
+
+    <h1 class="destaque2">HTML e PHP mesclados</h1>
+    <p>parágrafo HTML</p>
+
+    <?php
+        $linguagem = "Linguagem PHP.";
+    ?>
+    <p>Parágrafo mesclado HTML com <?=$linguagem?> </p>
+
+</body>
+</html>
